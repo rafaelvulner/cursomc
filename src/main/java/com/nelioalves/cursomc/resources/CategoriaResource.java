1173dfaa12bc7c442ebf9c5d9ec783d55dd231fc
Rafael Monteiro <rafaell.elias@hotmail.com>
2019-03-28 21:34:25 -0300

    Created file news

--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -3,28 +3,29 @@ package com.nelioalves.cursomc.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
 	
-	@GetMapping
-	public List<Categoria> listar() {
+	@Autowired
+	private CategoriaService service;
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> find(@PathVariable Integer id) {
 		
-		Categoria cat = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
+		Categoria cat = service.find(id);
 		
-		List<Categoria> list = new ArrayList<>();
-		
-		list.add(cat);
-		list.add(cat2);
-		
-		return list;
+		return ResponseEntity.ok(cat);
 	}
 
 }
